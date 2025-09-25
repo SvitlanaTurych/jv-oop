a72f9177721b2eb3fcd6726f17013545619db813
Svitlana Turych <svitlana.study24@gmail.com>
2025-09-25 19:08:44 +0300

    Created Truck class with overrided methods

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,12 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    @Override
+    public void doWork() {
+        System.out.println("Truck doWork");
+    }
+    @Override
+    public void stopWork() {
+        System.out.println("Truck stopWork");
+    }
+}
